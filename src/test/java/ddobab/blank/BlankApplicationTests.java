31e64dee84089d9f6c15936ce173a194cc6d5f03
youjin <developer_choi@naver.com>
2022-06-08 14:49:53 +0900

    project start

---
+++ src/test/java/ddobab/blank/BlankApplicationTests.java
@@ -0,0 +1,13 @@
+package ddobab.blank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
